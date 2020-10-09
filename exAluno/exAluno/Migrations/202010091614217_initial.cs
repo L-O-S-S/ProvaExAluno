03079b18ddfbf445e574035fe7f7257fe28ef725
LuisFelipeagarcia <57453710+LuisFelipeagarcia@users.noreply.github.com>
2020-10-09 14:39:25 -0300

    commit final

---
+++ exAluno/exAluno/Migrations/202010091614217_initial.cs
@@ -0,0 +1,31 @@
+﻿namespace exAluno.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.AlunoModel",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Nom = c.String(nullable: false),
+                        Curso = c.String(nullable: false),
+                        Fone = c.String(nullable: false),
+                        Endereco = c.String(nullable: false),
+                        Email = c.String(nullable: false),
+                        Fili = c.String(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.AlunoModel");
+        }
+    }
+}
